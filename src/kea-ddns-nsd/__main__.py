5bd34db3acb9440745a645422a195d07cc6022b8
Peter <peter1010@users.noreply.github.com>
2022-06-15 22:35:22 +0100

    Added __main__.py and __init__.py

---
+++ src/kea-ddns-nsd/__main__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+##
+# Copyright (c) 2022 Peter Leese
+#
+# Licensed under the GPL License. See LICENSE file in the project root for full license information.  
+##
+
+
+import os
+import logging
+
+
+def main():
+	pass
+
+
+if __name__ == "__main__":
+	main()
